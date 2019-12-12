10661e555dd311dd2489311886263abfebb1bfa8
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-12 04:28:44 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,6 +6,13 @@ block_return_values = []
   while i < collection.length
   yield(collection[i])
     i = i + 1 
+    end
+ 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
 
 my_all?([1,2,3]) {|i| i < 2}
